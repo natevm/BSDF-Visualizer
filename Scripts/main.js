9701c6571ea5e0ef9d58bc097b6015eed0dfaa03
silverLemons <5407450+silverLemons@users.noreply.github.com>
2018-02-09 10:53:48 -0800

    Now we have a rotating triangle

--- Scripts/main.js
+++ Scripts/main.js
@@ -72,17 +72,16 @@ var triangleArray = gl.createVertexArray();
 gl.bindVertexArray(triangleArray);
 
 const positions = new Float32Array([
-    -0.5, -0.5, -1.0,
-    0.5, -0.5, -1.0,
-    0.0, 0.5, -1.0
+    -0.5, -0.5, 0.0,
+    0.5, -0.5, 0.0,
+    0.0, 0.5, 0.0
 ]);
 
 var positionBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
 gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
-gl.enableVertexAttribArray(0);
-
+gl.enableVertexAttribArray(0); 
 const colors = new Float32Array([
     1.0, 0.0, 0.0,
     0.0, 1.0, 0.0,
@@ -95,10 +94,12 @@ gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW);
 gl.vertexAttribPointer(1, 3, gl.FLOAT, false, 0, 0);
 gl.enableVertexAttribArray(1);
 
-//Set up model, view, projection
+//Set up model, view
 M = mat4.create();
 V = mat4.create();
-//P = mat4.create()
+mat4.translate(V,V,[0, 0, -2]);
+
+//Perspective projection
 fov = Math.PI * 0.5;
 aspectRatio = 1; //TODO: get the actual width and height
 nearClip = 1;
@@ -108,22 +109,23 @@ P = MDN.perspectiveMatrix(fov, aspectRatio, nearClip, farClip);
 MV = mat4.create();
 MVP = mat4.create();
 
-/*
- *var then = 0;
- *var rot = 0;
- *var rot_angle = 0; //radians
- */
+var then = 0;
+var rot = 0;
+
+var rot_angle = 0; //radians
+var rot_axis = vec3.create();
+vec3.set(rot_axis, 0, 1, 0);
 
 function updateMVP(now){
 
-/*
- *  now *= 0.001; //convert to seconds
- *  var deltaTime = now - then;
- *  then = now;
- *
- *  var rotationSpeed = 1.2;
- *  rot_angle += rotationSpeed * deltaTime;
- */
+  now *= 0.001; //convert to seconds
+  var deltaTime = now - then;
+  then = now;
+
+  var rotationSpeed = 1.2;
+  rot_angle += rotationSpeed * deltaTime;
+
+  mat4.fromRotation(M, rot_angle, rot_axis);
 
   mat4.multiply(MV,V,M); //MV = V * M
   mat4.multiply(MVP,P,MV); //MVP = P * MV

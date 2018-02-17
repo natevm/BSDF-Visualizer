520634c2c09e463fc2c292cadb25067709c94a14
Nathan Marshak <5407450+n8xm@users.noreply.github.com>
2018-02-16 22:36:20 -0800

    Setting up programs is now in its own function

--- Scripts/main.js
+++ Scripts/main.js
@@ -42,33 +42,10 @@ gl.clearColor(0, 0, 0, 1);
 // SET UP PROGRAM
 /////////////////////
 
-const vsSource = document.getElementById("phong.vert").text.trim();
-const fsSource = document.getElementById("phong.frag").text.trim();
+const lobeVsSource = document.getElementById("phong.vert").text.trim();
+const lobeFsSource = document.getElementById("phong.frag").text.trim();
 
-var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-gl.shaderSource(vertexShader, vsSource);
-gl.compileShader(vertexShader);
-
-if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-    console.error(gl.getShaderInfoLog(vertexShader));
-}
-
-var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-gl.shaderSource(fragmentShader, fsSource);
-gl.compileShader(fragmentShader);
-
-if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-    console.error(gl.getShaderInfoLog(fragmentShader));
-}
-
-var program = gl.createProgram();
-gl.attachShader(program, vertexShader);
-gl.attachShader(program, fragmentShader);
-gl.linkProgram(program);
-
-if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-    console.error(gl.getProgramInfoLog(program));
-}
+var program = setup_program(lobeVsSource, lobeFsSource);
 
 gl.useProgram(program);
 

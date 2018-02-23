93f3e6b5d216dbb07d72160274ab051df31a977d
Nathan V. Morrical <bitinat2@isu.edu>
2018-02-23 14:14:51 -0700

    fixed model viewport

--- Scripts/ModelViewport.js
+++ Scripts/ModelViewport.js
@@ -2,7 +2,7 @@
 
 class ModelViewport {
   constructor(canvasName, width, height) {
-  	// the canvas2 element
+    // the canvas2 element
     this.canvas = document.getElementById(canvasName);
     this.canvas.width = 512;
     this.canvas.height = 512;
@@ -15,7 +15,15 @@ class ModelViewport {
 
     this.setupWebGL2();
     this.initShaders();
+
+    this.meshes = {};
+    this.models = {};
+    this.mvMatrix = mat4.create();
+    this.mvMatrixStack = [];
+    this.pMatrix = mat4.create();
+
     this.loadModels();
+    this.modelsLoaded = false;
   }
 
   setupWebGL2() {
@@ -26,10 +34,13 @@ class ModelViewport {
     }
     this.gl.clearColor(0, 0, 0, 1);
     this.gl.enable(this.gl.DEPTH_TEST);
+    this.gl.viewportWidth = this.canvas.width;
+    this.gl.viewportHeight = this.canvas.height;
+    this.gl.viewport(0, 0, this.canvas.width, this.canvas.height);
   }
 
   getShader(gl, id) {
-	var shaderScript = document.getElementById(id);
+  var shaderScript = document.getElementById(id);
     if (!shaderScript){
         return null;
     }
@@ -64,8 +75,8 @@ class ModelViewport {
   }
 
   initShaders() {
-  	var gl = this.gl;
-	var fragmentShader = this.getShader(gl, "shader-fs");
+    var gl = this.gl;
+  var fragmentShader = this.getShader(gl, "shader-fs");
     var vertexShader = this.getShader(gl, "shader-vs");
 
     this.shaderProgram = gl.createProgram();
@@ -104,51 +115,170 @@ class ModelViewport {
     this.shaderProgram.mvMatrixUniform = gl.getUniformLocation(this.shaderProgram, "uMVMatrix");
     this.shaderProgram.nMatrixUniform = gl.getUniformLocation(this.shaderProgram, "uNMatrix");
 
-    /* Not sure how to update this yet... */
-    // shaderProgram.applyAttributePointers = function(model) {
-    //     const layout = model.mesh.vertexBuffer.layout;
-    //     for (const attrName in attrs) {
-    //         if (!attrs.hasOwnProperty(attrName) || shaderProgram.attrIndices[attrName] == -1) {
-    //             continue;
-    //         }
-    //         const layoutKey = attrs[attrName];
-    //         if (shaderProgram.attrIndices[attrName] != -1) {
-    //             const attr = layout[layoutKey];
-    //             gl.vertexAttribPointer(
-    //                 shaderProgram.attrIndices[attrName],
-    //                 attr.size,
-    //                 gl[attr.type],
-    //                 attr.normalized,
-    //                 attr.stride,
-    //                 attr.offset);
-    //         }
-    //     }
-
-    // };
+    this.shaderProgram.applyAttributePointers = (model) => {
+        const layout = model.vertexBuffer.layout;
+        for (const attrName in attrs) {
+            if (!attrs.hasOwnProperty(attrName) || this.shaderProgram.attrIndices[attrName] == -1) {
+                continue;
+            }
+            const layoutKey = attrs[attrName];
+            if (this.shaderProgram.attrIndices[attrName] != -1) {
+                const attr = layout[layoutKey];
+                gl.vertexAttribPointer(
+                    this.shaderProgram.attrIndices[attrName],
+                    attr.size,
+                    gl[attr.type],
+                    attr.normalized,
+                    attr.stride,
+                    attr.offset);
+            }
+        }
+    };
   }
   
+  initBuffers() {
+    var layout = new OBJ.Layout(
+        OBJ.Layout.POSITION,
+        OBJ.Layout.NORMAL,
+        OBJ.Layout.DIFFUSE,
+        OBJ.Layout.UV,
+        OBJ.Layout.SPECULAR,
+        OBJ.Layout.SPECULAR_EXPONENT);
+
+    // initialize the mesh's buffers
+    for (var modelKey in this.models){
+        // Create the vertex buffer for this mesh
+        var vertexBuffer = this.gl.createBuffer();
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, vertexBuffer);
+        var vertexData = this.models[modelKey].makeBufferData(layout)
+        this.gl.bufferData(this.gl.ARRAY_BUFFER, vertexData, this.gl.STATIC_DRAW);
+        vertexBuffer.numItems = vertexData.numItems;
+        vertexBuffer.layout = layout;
+        this.models[modelKey].vertexBuffer = vertexBuffer;
+
+        // Create the index buffer for this mesh
+        var indexBuffer = this.gl.createBuffer();
+        this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+        var indexData = this.models[modelKey].makeIndexBufferData()
+        this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER, indexData, this.gl.STATIC_DRAW);
+        indexBuffer.numItems = indexData.numItems;
+        this.models[modelKey].indexBuffer = indexBuffer;
+
+        // this loops through the mesh names and creates new
+        // model objects and setting their mesh to the current mesh
+        //this.models[modelKey] = {};
+        //this.models[modelKey].mesh = this.models[modelKey];
+    }
+  }
+
+  setMatrixUniforms(){
+      this.gl.uniformMatrix4fv(this.shaderProgram.pMatrixUniform, false, this.pMatrix);
+      this.gl.uniformMatrix4fv(this.shaderProgram.mvMatrixUniform, false, this.mvMatrix);
+
+      var normalMatrix = mat3.create();
+      mat3.normalFromMat4(normalMatrix, this.mvMatrix);
+      this.gl.uniformMatrix3fv(this.shaderProgram.nMatrixUniform, false, normalMatrix);
+  }
+
   loadModels() {
-  	let p = OBJ.downloadModels([
+    let p = OBJ.downloadModels([
         {
+            name: 'teapot',
+            obj: 'models/teapot.obj',
+            mtl: true,
+        },
+        {
+            name: 'teapot2',
             obj: 'models/teapot.obj',
             mtl: true,
         },
     ]);
 
-    p.then((models) => {
-    	console.log(models["teapot"]);
-    	console.log('Name:', models["teapot"].name);
-        console.log('Mesh:', models["teapot"]);
-    	
-    	/* For some reason this is giving me trouble. Seems to work on other browsers...*/
-        // for ([name, mesh] of Object.entries(models)) {
+    //  (meshes) => {
+    //   console.log(models["teapot"]);
+    //   console.log('Name:', models["teapot"].name);
+    //     console.log('Mesh:', models["teapot"]);
+      
+    //   /* For some reason this is giving me trouble. Seems to work on other browsers...*/
+    //     // for ([name, mesh] of Object.entries(models)) {
         
-        // }
-        this.models = models;
+    //     // }
+    //     this.models = models;
+    //     this.modelsLoaded = true;
+
+    //     /* Now that the models are loaded, we can initialize the buffers */
+    //     this.initBuffers();
+    // });
+
+    p.then((models) => {
+      console.log(models);
+      this.models = models;
+      this.initBuffers();
+      this.modelsLoaded = true;
     });
   }
 
+  mvPushMatrix(){
+      var copy = mat4.create();
+      mat4.copy(copy, this.mvMatrix);
+      this.mvMatrixStack.push(copy);
+  }
+
+  mvPopMatrix(){
+      if (this.mvMatrixStack.length === 0){
+          throw "Invalid popMatrix!";
+      }
+      this.mvMatrix = this.mvMatrixStack.pop();
+  }
+
+  drawObject(model){
+      /*
+       Takes in a model that points to a mesh and draws the object on the scene.
+       Assumes that the setMatrixUniforms function exists
+       as well as the shaderProgram has a uniform attribute called "samplerUniform"
+       */
+  //    gl2.useProgram(shaderProgram);
+
+      this.gl.bindBuffer(this.gl.ARRAY_BUFFER, model.vertexBuffer);
+      this.shaderProgram.applyAttributePointers(model);
+
+      this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, model.indexBuffer);
+      this.setMatrixUniforms();
+      this.gl.drawElements(this.gl.TRIANGLES, model.indexBuffer.numItems, this.gl.UNSIGNED_SHORT, 0);
+  }
+
+  drawScene() {
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+    mat4.perspective(this.pMatrix, 45 * Math.PI / 180.0, this.gl.viewportWidth / this.gl.viewportHeight, 0.01, 1000.0);
+    mat4.identity(this.mvMatrix);
+    // move the camera
+    mat4.translate(this.mvMatrix, this.mvMatrix, [0, -10, -40]);
+    mat4.rotate(this.mvMatrix, this.mvMatrix, -0.5 * Math.PI, [1, 0, 0]);
+    mat4.rotate(this.mvMatrix, this.mvMatrix, this.time * 0.25 * Math.PI, [0, 0, 1]);
+    // set up the scene
+    this.mvPushMatrix();
+        this.drawObject(this.models.teapot);
+    this.mvPopMatrix();
+  }
+
+  animate() {
+    this.timeNow = new Date().getTime();
+    this.elapsed = this.timeNow - this.lastTime;
+    if (!this.time) {
+        this.time = 0.0;
+    }
+    this.time += this.elapsed / 1000.0;
+    if (this.lastTime !== 0){
+        // do animations
+    }
+    this.lastTime = this.timeNow;
+  }
+
   render(time) {
-  	this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+    if (this.modelsLoaded) {
+      this.drawScene();
+      this.animate();
+    }
   }
 }

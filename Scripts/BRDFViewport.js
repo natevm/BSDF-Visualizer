9d7db77d13021edef915f6c36307d4353eafc8a9
n8xm <n8xm@protonmail.com>
2018-03-17 18:39:35 -0700

    Now lobe orientation is correct after loading analytical BRDF

--- Scripts/BRDFViewport.js
+++ Scripts/BRDFViewport.js
@@ -322,7 +322,7 @@ export default function BRDFViewport(spec) {
     },
 
     setupGeometry = function() {
-      let L_hat = compute_L_hat(in_theta_deg, in_phi_deg + 180);
+      let L_hat = compute_L_hat(in_theta_deg, in_phi_deg);
       let N_hat = compute_N_hat();
 
       lobeVAO = gl.createVertexArray();
